b2963e0855dd922211cd92adacbd2a6709a42e01
zbw322092 <bz4e14@soton.ac.uk>
2017-03-13 23:29:03 +0800

    process response

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ var nativeProtocols = {
 	'http:': http,
 	'https:': https,
 };
+var safeMethods = {
+	GET: true, 
+	HEAD: true, 
+	OPTIONS: true, 
+	TRACE: true
+};
 
 var eventHandles = Object.create(null);
 ['abort', 'aborted', 'error', 'socket'].forEach(function (event) {
@@ -73,6 +79,49 @@ RedirectableRequest.prototype._performRequest = function() {
 		}
 	}
 
+};
+
+RedirectableRequest.prototype._processResponse = function(response) {
+	var location = response.headers.location;
+	if (location && this._options.followRedirects !== false &&
+		response.statusCode >= 300 && response.statusCode < 400) {
+		if (++this._redirectCount > this._options.maxRedirects) {
+			return this.emit('error', new Error('Max redirects exceeded.'));
+		}
+
+		var header;
+		var header = this._options.headers;
+		if (response.statusCode !== 307 && !(this._options.method in safeMethods)) {
+			this._options.method = 'GET';
+
+			this._bufferedWrites = [];
+			for (header in headers) {
+				if (/^content-/i.test(header)) {
+					delete headers[header];
+				}
+			}
+		}
+
+		if (!this._isRedirect) {
+			for (header in headers) {
+				if (/^host$/i.test(header)) {
+					delete headers[header];
+				}
+			}
+		}
+
+		var redirectUrl = url.resolve(this._currentUrl, location);
+
+		Object.assign(this._options, url.parse(redirectUrl));
+		this._isRedirect = true;
+		this._performRequest();
+	} else {
+		response.responseUrl = this._currentUrl;
+		this.emit('response', response);
+
+		delete this._options;
+		delete this._bufferedWrites;
+	}
 };
 
 
