df6c4840ec1a6a4416ee8060c1d4c5c4f95d8edc
zbw322092 <bz4e14@soton.ac.uk>
2017-03-13 23:13:33 +0800

    performRequest

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var http = require('http');
 var https = require('https');
 var Writable = require('stream').Readable;
 
+var nativeProtocols = {
+	'http:': http,
+	'https:': https,
+};
+
+var eventHandles = Object.create(null);
+['abort', 'aborted', 'error', 'socket'].forEach(function (event) {
+	eventHandles[event] = function (arg) {
+		this._redirectable.emit(event, arg);
+	};
+});
+
+
 function RedirectableRequest(options, responseCallback) {
 	Writable.call(this);
 	this._options = options;
@@ -23,3 +36,68 @@ function RedirectableRequest(options, responseCallback) {
 }
 
 RedirectableRequest.prototype = Object.create(Writable.prototype);
+
+RedirectableRequest.prototype._performRequest = function() {
+	var protocol = this._options.protocol;
+	// if (this._options.agents) {
+	// 	this._options.agent = this._options.agents[]
+	// }
+
+	var nativeProtocol = nativeProtocols[this._options.protocol];
+	var request = this._currentRequest = 
+		nativeProtocol.request(this._options, this._onNativeResponse);
+
+	this._currentUrl = url.format(this._options);
+
+	request._redirectable = this;
+	for (var event in eventHandles) {
+		if (event) {
+			request.on(event, eventHandles[event]);
+		}
+	}
+
+	if (this._isRedirect) {
+		var bufferedWrites = this._bufferedWrites;
+		if (bufferedWrites.length === 0) {
+			request.end();
+		} else {
+			var i = 0;
+			(function writeNext() {
+				if (i < bufferedWrites.length) {
+					var bufferedWrite = bufferedWrites[i++];
+					request.write(bufferedWrite.data, bufferedWrite.encoding, writeNext);
+				} else {
+					request.end();
+				}
+			})();
+		}
+	}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
