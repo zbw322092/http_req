9157701d0cb2b9a7eeba3d472fd4824fdfb23b92
zbw322092 <bz4e14@soton.ac.uk>
2017-03-14 21:10:54 +0800

    net server

--- examples/net.server.js
+++ examples/net.server.js
@@ -6,6 +6,27 @@ var server = net.createServer((socket) => {
 	throw err;
 });
 
-server.listen(() => {
+server.listen({
+	port: 1337
+} ,() => {
 	console.log('opened server on', server.address());
+});
+
+server.on('connection', (socket) => {
+	server.getConnections((err, count) => {
+		if (err) throw err;
+		console.log('connections amount:', count);
+	});
+});
+
+server.on('error', (e) => {
+	if (e.code === 'EADDRINUSE') {
+		console.log('Address in use, retrying...');
+		setTimeout(() => {
+			server.close();
+			server.listen(() => {
+				console.log('opened server on', server.address());
+			});
+		}, 1000);
+	}
 });
